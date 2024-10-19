2a0cbd9d1998178e275cfbd5b428f08e33ff3453
SRIRAM <boddusriram1234@gmail.com>
2024-10-20 03:04:23 +0530

    Update admin.py

--- backend/main/admin.py
+++ backend/main/admin.py
@@ -1,6 +1,4 @@
 from django.contrib import admin
-from . import models
-# Register your models here.
+from .models import USER, VideoAnalysis
 
-
-admin.site.register(models.USER)
+admin.site.register(VideoAnalysis)
