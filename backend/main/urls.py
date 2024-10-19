3412c47810d3f12cd4ba44ac9a810272a460a47c
SRIRAM <boddusriram1234@gmail.com>
2024-10-20 03:05:50 +0530

    Update urls.py

--- backend/main/urls.py
+++ backend/main/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('users/', views.UserList.as_view()),
-    path('users/<int:pk>/', views.UserDetail.as_view()),
+    path('users/', views.UserList.as_view(), name='user-list'),
+    path('users/<int:pk>/', views.UserDetail.as_view(), name='user-detail'),
+    path('detect_deepfake/', views.detect_deepfake, name='detect_deepfake'),
+    path('video-analyses/', views.VideoAnalysisList.as_view(), name='video-analysis-list'),
+    path('video-analyses/<int:pk>/', views.VideoAnalysisDetail.as_view(), name='video-analysis-detail'),
 ]
